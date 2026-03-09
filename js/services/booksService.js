a91e02901f2f8dcacee453ea209082826b32c8e0
Richard-AA-pr <rer00@iesemilidarder.com>
2026-03-09 12:23:25 +0100

    organizacion js

--- js/services/booksService.js
+++ js/services/booksService.js
@@ -2,16 +2,16 @@ export async function loadBooks() {
 
   try {
 
-    const r = await fetch("data/books.json");
+    const r = await fetch("../../data/books.json");
 
     if (!r.ok) throw new Error("Error cargando datos");
 
     return await r.json();
 
-  }
-  catch (e) {
+  } catch (e) {
 
     console.error(e);
+
     return [];
 
   }
